0cce9bc549c622787f6af26e18552b8cb9cf1e09
liuz <liuzhuan@qingsongchou.com>
2017-08-02 18:04:47 +0800

    add comments

--- myscripts/check-gzip.sh
+++ myscripts/check-gzip.sh
@@ -2,6 +2,17 @@
 
 # check if server has open gzip or not
 
+# Usage: 
+# $ check-gzip.sh your-target-server
+# => [your-target-server]: <uncompressed size> => <compressed size>
+
+# If <uncompressed size> equals <compressed size>, the server hasn't open
+# gzip yet, else it has a working gzip function.
+
+# Scripts based on [zoul's answer on stackoverflow](https://stackoverflow.com/questions/9140178/how-can-i-tell-if-my-server-is-serving-gzipped-content)
+
+# This script depends on [curl](https://curl.haxx.se/) command line tool.
+
 SITE=$1
 
 echo 'analysing...'
